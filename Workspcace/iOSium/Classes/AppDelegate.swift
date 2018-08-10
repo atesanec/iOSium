378127ce926f96def776ef825ad5229bb28a394a
V <v>
2018-08-10 17:29:01 +0300

    Added empty project

---
+++ Workspcace/iOSium/Classes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iOSium
+//
+//  Created by VI_Business on 10/08/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
